cca16dc1e93ad36f90f2ed37207b41e8dabcac85
Carlos Alberto Martins <betoxz@gmail.com>
2022-09-05 10:24:57 -0300

    GFTTest

---
+++ Trade.Domain/TradeContext/Entities/Trade.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Trade.Domain.TradeContext.Enuns;
+using Trade.Domain.TradeContext.Intefaces;
+
+namespace Trade.Domain.TradeContext.Entities
+{
+    public class Trade : ITrade
+    {
+        public double Value { get; private set; }
+        public string ClientSector { get; private set; }
+        public DateTime NextPaymentDate { get; }
+
+        public Trade(double valor, string clienteSetor, DateTime NextPaymentDate)
+        {
+            this.Value = valor;
+            this.ClientSector = clienteSetor;
+            this.NextPaymentDate = NextPaymentDate;
+        }
+
+        public string getCateroria()
+        {
+            return (this.ClientSector.Equals(SetorEnum.Private.ToString())) ?
+                new SetorPrivado().getCategoria(this.Value)
+                : new SetorPublico().getCategoria(this.Value);
+        }
+    }
+}
---
+++ Trade.Domain/TradeContext/Intefaces/ITrade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trade.Domain.TradeContext.Intefaces
+{
+    interface ITrade
+    {
+        double Value { get; }
+        string ClientSector { get; }
+        DateTime NextPaymentDate { get; }
+    }
+}
